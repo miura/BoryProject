b3906f95f770ea971824a9da30ef84afe65556ca
kotamiura <kotamiura@0ce82a30-2411-ce4f-b84c-be783ca2c29f>
2010-05-25 15:41:10 +0000

    added Object4D class

---
+++ src/emblcmci/Object4D.java
@@ -0,0 +1,14 @@
+package emblcmci;
+
+import Utilities.Object3D;
+
+public class Object4D extends Object3D{
+	
+	public int timepoint;
+	
+	public Object4D(int size) {
+		super(size);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
