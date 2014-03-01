33a4d1371e6f2af18d0426e0125dc4bbf44a1b91
schiklen <schiklen@embl.de>
2014-03-01 12:31:45 +0100

    Added getParameters method

--- src/main/java/emblcmci/foci3Dtracker/AutoThrAdj3D_rewrite.java
+++ src/main/java/emblcmci/foci3Dtracker/AutoThrAdj3D_rewrite.java
@@ -26,25 +26,56 @@ import ij.process.StackProcessor;
 
 public class AutoThrAdj3D_rewrite {
 
-	int maxspotvoxels;
-	int minspotvoxels;
+	// Segmentation parameters
+	int maxspotvoxels, minspotvoxels;
 	int minspotvoxels_measure;
 	int maxloops;
-	int thadj_volmin;
-	int thadj_volmax;
-	int thadj_nummin;
+	int thadj_volmin, thadj_volmax;
+	int thadj_nummin, thadj_nummax;
 
 	
-	/*Constructor with segmentation parameters as arguments
-	 * 
-	 */
+	private static boolean createComposite = true;
+	int thr, minSize, maxSize, dotSize, fontSize;
+	boolean excludeOnEdges, showObj, showSurf, showCentro, showCOM, showNb, whiteNb, newRT, showStat, closeImg, showSummary, redirect;
+	boolean silent = false;
+	boolean showMaskedImg = false;
+	
+	/*Constructors*/
+	//Empty constructor
+	public AutoThrAdj3D_rewrite(){} // do I really need  or can I construct without arguments anyway?!?
+	
+	//Constructor with segmentation parameters as arguments
 	public AutoThrAdj3D_rewrite(int maxspotvoxels, 
 								int minspotvoxels,
 								int minspotvoxels_measure,
 								int maxloops,
 								int thadj_volmin,
 								int thadj_volmax,
-								int thadj_nummin){
+								int thadj_nummin,
+								int thadj_nummax){
 		this.maxspotvoxels = maxspotvoxels;
+		this.minspotvoxels = minspotvoxels;
+		this.minspotvoxels_measure = minspotvoxels_measure;
+		this.maxloops = maxloops;
+		this.thadj_volmin = thadj_volmin;
+		this.thadj_volmax = thadj_volmax;
+		this.thadj_nummin = thadj_nummin;
+		this.thadj_nummax = thadj_nummax;
 	}
+	
+	/*Methods*/
+	public void setParameters(){}
+	
+	// Just a convenient method to later retrieve the parameter fields with one line.
+	public int[] getParameters(){
+		int[] parameters = { 
+				this.maxspotvoxels, this.minspotvoxels, this.minspotvoxels_measure,
+				this.maxloops,
+				this.thadj_volmin, this.thadj_volmax,
+				this.thadj_nummin, this.thadj_nummax
+			};
+		return parameters;
+		}
+
+
 }
